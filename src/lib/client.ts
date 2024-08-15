70e75ece43c1cdcd6b134f8aa4aab1a78e5a25f4
takaya787 <takaya318318@gmail.com>
2024-08-16 00:12:01 +0900

    認証を設定

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,16 +2,7 @@ import { DynamoDB } from '@aws-sdk/client-dynamodb';
 
 import Dynamode from 'dynamode/dynamode';
 
+// Local以外ではIAM Roleから認証情報を取得する
 export const dynamodeDBClient = process.env.AWS_SAM_LOCAL
     ? Dynamode.ddb.local('http://host.docker.internal:8000')
-    : Dynamode.ddb.set(
-          new DynamoDB({
-              region: 'ap-northeast-1',
-              credentials: {
-                  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-                  accessKeyId: 'accessKeyId',
-                  secretAccessKey: 'secretAccessKey',
-              },
-          }),
-      );
+    : Dynamode.ddb.get();
