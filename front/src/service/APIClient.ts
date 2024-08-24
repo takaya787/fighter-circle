d3ac6212639876d0d722b10ffd03378a806ed1e9
takaya787 <takaya318318@gmail.com>
2024-08-24 18:02:12 +0900

    credentialを追加

--- front/src/service/APIClient.ts
+++ front/src/service/APIClient.ts
@@ -19,6 +19,7 @@ export class APIClient {
                     Accept: 'application/json',
                     Authorization: `Bearer ${this.token}`,
                     'Content-Type': 'application/json',
+                    withCredentials: true,
                 },
             });
 
@@ -41,6 +42,7 @@ export class APIClient {
                     Authorization: `Bearer ${this.token}`,
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
+                    withCredentials: true,
                 },
             });
 
